56c9354d60f63b14f45c353292602e4253ac8427
Manya <153523246+Manya98@users.noreply.github.com>
2024-08-04 12:17:13 +0000

    container config done

---
+++ color-changer/index.js
@@ -0,0 +1,25 @@
+console.log("Manya")
+
+console.log("Project-1")
+
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+buttons.forEach(function (button){
+    console.log(button);
+    button.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'pink'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
